1193c7a973e9d31cd40201de2f69977e69a62d97
Jlad <lanetejlad@gmail.com>
2024-04-15 20:32:40 +0800

    upload profile research

--- resources/views/components/profile-logo.blade.php
+++ resources/views/components/profile-logo.blade.php
@@ -1,14 +1,18 @@
 @props([
     'src',
     'alt',
+    'id' => 'profile',
     'width' => 'w-32',
-    'height' => 'h-32'
+    'height' => 'h-32',
+    'accept' => 'image/jpg, image/png, image/jpeg'
 ])
-<figure>
-    <input type="file" onchange="preview()" accept="image/*" name="upload">
+<figure class="w-fit text-center">
     <img 
     src= "{{$src}}"
     alt= "{{$alt}}"
-    {{$attributes->merge(['class' => "$width $height rounded-full"])}}
+    id ="{{$id}}"
+    {{$attributes->merge(['class' => "$width $height rounded-full my-3"])}}
     >
+    <label for="profile-pic" class="bg-red-800 py-2 px-3 rounded-full text-center text-white">Upload Image</label>
+    <input type="file" accept="$accept" name="profile-pic" class="mt-3 hidden" id="profile-pic">
 </figure>
